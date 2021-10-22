2d394e4af2f97c6bb962bd1832544aa655a667ef
jasfa <60039036+jasfa@users.noreply.github.com>
2021-10-22 23:16:28 +0800

    Create modelselection.js

---
+++ pages/modelselection/modelselection.js
@@ -0,0 +1,10 @@
+// pages/modelselection/modelselection.js
+Page({
+  govs(){
+    console.log("点击了按钮")
+    wx.navigateTo({
+      url: '/pages/renren/renren',
+    })
+    console.log("跳转成功")
+  }
+})
